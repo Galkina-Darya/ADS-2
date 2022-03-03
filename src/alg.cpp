c3c48123dfaebb5275b7bffca798e17ef69cadb7
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-03-04 00:54:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,8 +14,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-   if (n == 0)
-        return 1;
+  if (n == 0) return 1;
     return n * fact(n - 1);
 }
 
@@ -26,7 +25,7 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
- double e = 0;
+  double e = 0;
     for (uint16_t i = 0; i <= count; i++) {
         e += calcItem(x, i);
     }
@@ -34,7 +33,7 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
- double sn = 0;
+  double sn = 0;
     for (uint16_t i = 1; i <= count; i++) {
         sn += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
     }
